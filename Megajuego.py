fad8302575aa52b70cb348d4564ecaa13dfec52f
ChaferMonster <alvarogarciachafer8@gmail.com>
2025-02-19 12:27:38 +0100

    Mejora de funciones de ataque

--- Megajuego.py
+++ Megajuego.py
@@ -2,13 +2,13 @@ import tkinter
 
 class personaje():
     def __init__(self, nombre, vida, vivo, ataque, defensa, suerte, descripcion):
-        self.nombre = nombre
-        self.vida = vida
-        self.vivo = vivo
-        self.ataque = ataque
-        self.defensa = defensa
-        self.suerte = suerte
-        self.descripcion = descripcion
+        self.nombre = nombre #nombre del personaje
+        self.vida = vida #vida del personaje
+        self.vivo = vivo #Indica si el personaje esta vivo
+        self.ataque = ataque #potencia de ataque del personaje
+        self.defensa = defensa #potencia que resiste el personaje en cada golpe
+        self.suerte = suerte #número que se suma a la potencia en cada movimiento para añadirle fuerza al ataque
+        self.descripcion = descripcion #descripción del personaje
         pass
 
     def muerto(self): #Define que el personaje muere
@@ -29,17 +29,17 @@ class tanque(personaje):
 
     def ataque_1(self): #Ataque de personaje
         nombre_ataque = "Escudo antidarius"
-        potencia = 50 + (self.suerte)
+        potencia = (50 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_2(self): #Ataque de personaje
         nombre_ataque = "Pisotón antidario"
-        potencia = 40 + (self.suerte)
+        potencia = (40 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Quebrantadarios"
-        potencia = 60 + (self.suerte)
+        potencia = (60 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
 
@@ -51,17 +51,17 @@ class asesino(personaje):
 
     def ataque_1(self):#Ataque de personaje
         nombre_ataque = "Ruleta de la suerte asesina"
-        potencia = 20 + (self.suerte)
+        potencia = (20 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_2(self):#Ataque de personaje
         nombre_ataque = "DagametidapordetrasaDario"
-        potencia = 10 + (self.suerte + self.suerte)
+        potencia = (10 + (self.suerte + self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Lluvia de las millones de dagas"
-        potencia = 40 + (self.suerte)
+        potencia = (40 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
 
@@ -73,17 +73,17 @@ class heroe(personaje):
 
     def ataque_1(self):#Ataque de personaje
         nombre_ataque = "Espada penetradora. Pobre Dario"
-        potencia = 60 + (self.suerte)
-        return potencia, nombre_ataque
+        potencia = (60 + (self.suerte)) * self.ataque
+        return potencia, nombre_ataque 
 
     def ataque_2(self):#Ataque de personaje
         nombre_ataque = "Puño del valor"
-        potencia = 30 + (self.suerte)
+        potencia = (30 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Doble espada penetradora"
-        potencia = 10 + (self.suerte + self.suerte)
+        potencia = (10 + (self.suerte + self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
 
@@ -94,17 +94,17 @@ class mago(personaje):
 
     def ataque_1(self):#Ataque de personaje
         nombre_ataque = "Bola de permatrago"
-        potencia = 30 + (self.suerte)
+        potencia = (30 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_2(self):#Ataque de personaje
         nombre_ataque = "Rayo congelabolas"
-        potencia = 30 + (self.suerte)
+        potencia = (30 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_3(self):#Ataque de personaje
         nombre_ataque = "MAEB"
-        potencia = 150
+        potencia = 150 * self.ataque
         return potencia, nombre_ataque
 
 
@@ -114,23 +114,23 @@ class dario(personaje):
         self.desgaste = desgaste
 
     def ataque_1(self):#Ataque de personaje
-        nombre_ataque = "FIIIIIU"
-        potencia = 50 + (self.suerte)
+        nombre_ataque = "Silbido: FIIIIIU"
+        potencia = (50 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_2(self):#Ataque de personaje
         nombre_ataque = "Aplausos"
-        potencia = 30 + (self.suerte)
+        potencia = (30 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Quejas, quejas y más quejas"
-        potencia = 10 + (self.suerte + self.suerte)
+        potencia = (10 + (self.suerte + self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
     def ataque_4(self):#Ataque de personaje
         nombre_ataque = "ME F*LLO A TU MADRE"
-        potencia = 80 + (self.suerte)
+        potencia = (80 + (self.suerte)) * self.ataque
         return potencia, nombre_ataque
 
 
