dfd08a39e16551019d2a8498c31758334f42bb09
ChaferMonster <alvarogarciachafer8@gmail.com>
2025-02-19 12:01:43 +0100

    Añadido de métodos muerto y aparecer

--- Megajuego.py
+++ Megajuego.py
@@ -12,6 +12,16 @@ class personaje():
         self.descripcion = descripcion
         pass
 
+    def muerto(self): #Define que el personaje muere
+        if self.vida <= 0:
+            self.vivo == False
+
+    def aparecer(self): #Define si el personaje aparece como muerto o como vivo en el menú de selección
+        if self.vivo == True:
+            return True
+        else:
+            return False
+
 
 
 class tanque(personaje):
@@ -19,15 +29,15 @@ class tanque(personaje):
         super().__init__(nombre, vida, estamina, vivo, ataque, defensa, tipo, suerte, descripción)
         self.megadefensa = megadefensa
 
-    def ataque_1(self):
+    def ataque_1(self): #Ataque de personaje
         nombre_ataque = "Escudo antidarius"
         potencia = 50 + (self.suerte)
 
-    def ataque_2(self):
+    def ataque_2(self): #Ataque de personaje
         nombre_ataque = "Pisotón antidario"
         potencia = 40 + (self.suerte)
 
-    def ataque_3(self):
+    def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Quebrantadarios"
         potencia = 60 + (self.suerte)
 
@@ -38,15 +48,15 @@ class asesino(personaje):
         self.evasion = evasion
         self.critico = critico
 
-    def ataque_1(self):
+    def ataque_1(self):#Ataque de personaje
         nombre_ataque = "Ruleta de la suerte asesina"
         potencia = 20 + (self.suerte)
 
-    def ataque_2(self):
+    def ataque_2(self):#Ataque de personaje
         nombre_ataque = "DagametidapordetrasaDario"
         potencia = 10 + (self.suerte + self.suerte)
 
-    def ataque_3(self):
+    def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Lluvia de las millones de dagas"
         potencia = 40 + (self.suerte)
 
@@ -57,15 +67,15 @@ class heroe(personaje):
         self.critico = critico
         self.golpe_valor = golpe_valor
 
-    def ataque_1(self):
+    def ataque_1(self):#Ataque de personaje
         nombre_ataque = "Espada penetradora. Pobre Dario"
         potencia = 60 + (self.suerte)
 
-    def ataque_2(self):
+    def ataque_2(self):#Ataque de personaje
         nombre_ataque = "Puño del valor"
         potencia = 30 + (self.suerte)
 
-    def ataque_3(self):
+    def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Doble espada penetradora"
         potencia = 10 + (self.suerte + self.suerte)
 
@@ -75,15 +85,15 @@ class mago(personaje):
         super().__init__(nombre, vida, estamina, vivo, ataque, defensa, suerte, descripcion)
         self.poder_magico = poder_magico
 
-    def ataque_1(self):
+    def ataque_1(self):#Ataque de personaje
         nombre_ataque = "Bola de permatrago"
         potencia = 30 + (self.suerte)
 
-    def ataque_2(self):
+    def ataque_2(self):#Ataque de personaje
         nombre_ataque = "Rayo congelabolas"
         potencia = 30 + (self.suerte)
 
-    def ataque_3(self):
+    def ataque_3(self):#Ataque de personaje
         nombre_ataque = "MAEB"
         potencia = 150
 
@@ -93,19 +103,19 @@ class dario(personaje):
         super().__init__(nombre, vida, estamina, vivo, ataque, defensa, suerte, descripcion)
         self.desgaste = desgaste
 
-    def ataque_1(self):
+    def ataque_1(self):#Ataque de personaje
         nombre_ataque = "FIIIIIU"
         potencia = 50 + (self.suerte)
 
-    def ataque_2(self):
+    def ataque_2(self):#Ataque de personaje
         nombre_ataque = "Aplausos"
         potencia = 30 + (self.suerte)
 
-    def ataque_3(self):
+    def ataque_3(self):#Ataque de personaje
         nombre_ataque = "Quejas, quejas y más quejas"
         potencia = 10 + (self.suerte + self.suerte)
 
-    def ataque_4(self):
+    def ataque_4(self):#Ataque de personaje
         nombre_ataque = "ME F*LLO A TU MADRE"
         potencia = 80 + (self.suerte)
 
